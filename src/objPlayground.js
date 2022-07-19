aaed8a4bf9b1c4662f4515d4ce3512eb574a945e
Wendel Daniel <wendeldaniel263@gmail.com>
2022-07-19 19:52:30 -0300

    correção de lint 2

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -37,7 +37,7 @@
 const calculator = (number1, number2) => ({
   sum: number1 + number2,
   mult: number1 * number2,
-  div: Math.floor (number1 / number2),
+  div: Math.floor(number1 / number2),
   sub: number1 - number2,
 });
 
@@ -48,7 +48,7 @@ const arrayGenerator = (type, object) => {
   if (type === 'values') {
     return Object.values(object);
   }
-  if (type === 'entries'){
+  if (type === 'entries') {
     return Object.entries(object);
   }
 };
