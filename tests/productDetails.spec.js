84d189c33d30c6c2f695a75e1434a37df7aca965
Wendel Daniel <wendeldaniel263@gmail.com>
2022-07-20 13:28:03 -0300

    exercicio 6 ok

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -43,11 +43,27 @@ describe('6 - Implemente os casos de teste para a função `productDetails`', ()
   it('Teste se o array retornado pela função contém dois itens dentro.', () => {
     expect(productDetails([]).length).toBe(2)
   });
-  // it('Teste se os dois itens dentro do array retornado pela função são objetos.', () => {
-  //   expect(productDetails([]).length).toBe(2)
-  // });
+  it('Teste se os dois itens dentro do array retornado pela função são objetos.', () => {
+    expect(typeof productDetails()).toBe('object')
+  });
+  it('Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.', () => {
+    const expectedOutput1 = [
+      { name: 'agua', details: { productId: 'agua123' } },
+      { name: '', details: { productId: '123' } }
+    ]
+    const expectedOutput2 = [
+      { name: '', details: { productId: '123' } },
+      { name: 'rocha', details: { productId: 'rocha123' } }
+    ]
+    expect(productDetails('agua', '')).toEqual(expectedOutput1);
+    expect(productDetails('', 'rocha')).toEqual(expectedOutput2);
+  })
+  // it('Teste se os dois productIds terminam com 123.', () => {
+  //   const expectedOutput = expect.stringMatching('123');
+  //   expect(productDetails('agua', 'rocha')).toEqual(
+  //     expect.arrayContaining(expectedOutput),
+  //   )
+  // })
   // ESCREVA SEUS TESTES ABAIXO:
     // 
-    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
-    // Teste se os dois productIds terminam com 123.
 });
